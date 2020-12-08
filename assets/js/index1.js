dd317ce39374a005d7803b7293b088052adfa22f
Daniel Jama <djama@abt.com.local>
2020-12-08 15:38:42 -0600

    adjusting to inline js

--- assets/js/index1.js
+++ assets/js/index1.js
@@ -1,74 +1,23 @@
-import * as CrComLib from "@crestron/ch5-crcomlib";
+(function(CrComLib){
+  var btn1=document.querySelector('#ex1-btn1');
+  var btn2=document.querySelector('#ex1-btn2');
 
-const launchBtnA = document.getElementById("launchBtnA");
-const launchBtnB = document.getElementById("launchBtnB");
-const launchBtnC = document.getElementById("launchBtnC");
-const mainBtnA = document.getElementById("mainBtnA");
-const mainBtnB = document.getElementById("mainBtnB");
-const mainBtnC = document.getElementById("mainBtnC");
+  btn1.addEventListener('click',btn1Click);
+  btn2.addEventListener('click',btn2Click);
 
-launchBtnA.addEventListener("click", () => {
-  location.href = "localPC.html";
-  console.log("PC button has been pushed");
-  CrComLib.publishEvent("b", "1", "true");
-  console.log("1 High")
-    setTimeout(() => {
-      CrComLib.publishEvent("b", "1", "false");
-    }, 2000);
-    console.log("1 Low")
-});
+  function btn1Click(){
+      CrComLib.publishEvent('b','ex1_b',true);
+      CrComLib.publishEvent('n','ex1_n',3);
+      CrComLib.publishEvent('s','ex1_s','New York');
+      
+      
+  }
 
-launchBtnB.addEventListener("click", function () {
-location.href = "cable.html";
-console.log("Cable button has been pushed");
-CrComLib.publishEvent("b", "2", "true");
-console.log("2 High")
-  setTimeout(() => {
-    CrComLib.publishEvent("b", "2", "false");
-  }, 2000);
-  console.log("2 Low")
-});
-
-launchBtnC.addEventListener("click", function () {
-location.href = "appleTV.html";
-console.log("Apple button has been pushed");
-CrComLib.publishEvent("b", "3", "true");
-console.log("3 High")
-  setTimeout(() => {
-    CrComLib.publishEvent("b", "3", "false");
-  }, 2000);
-  console.log("3 Low");
-});
-
-// mainBtnA.addEventListener("click", () => {
-//   location.href = "localPC.html";
-//   console.log("PC button has been pushed");
-//   CrComLib.publishEvent("b", "1", "true");
-//   console.log("1 High")
-//     setTimeout(() => {
-//       CrComLib.publishEvent("b", "1", "false");
-//     }, 2000);
-//     console.log("1 Low")
-// });
-
-// mainBtnB.addEventListener("click", function () {
-// location.href = "cable.html";
-// console.log("Cable button has been pushed");
-// CrComLib.publishEvent("b", "2", "true");
-// console.log("2 High")
-//   setTimeout(() => {
-//     CrComLib.publishEvent("b", "2", "false");
-//   }, 2000);
-//   console.log("2 Low")
-// });
-
-// mainBtnC.addEventListener("click", function () {
-// location.href = "appleTV.html";
-// console.log("Apple button has been pushed");
-// CrComLib.publishEvent("b", "3", "true");
-// console.log("3 High")
-//   setTimeout(() => {
-//     CrComLib.publishEvent("b", "3", "false");
-//   }, 2000);
-//   console.log("3 Low");
-// });
+  function btn2Click(){
+      CrComLib.publishEvent('b','ex1_b',false);
+      CrComLib.publishEvent('n','ex1_n',11);
+      CrComLib.publishEvent('s','ex1_s','London');
+      
+      
+  }
+})(CrComLib);
